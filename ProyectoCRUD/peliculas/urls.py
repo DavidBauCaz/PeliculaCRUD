fc1e20865b0a249868b37c33e489a45191db0c82
TripleD61 <ingediaz10@gmail.com>
2025-01-23 16:54:39 -0600

    Se agregaron las funcionalidades de read y create de las peliculas

--- ProyectoCRUD/peliculas/urls.py
+++ ProyectoCRUD/peliculas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from .views import ListarPeliculas, DetallesPelicula, CrearPelicula, ActualizarPelicula, EliminarPelicula
 
 urlpatterns = [
-    path('', views.index, name='index')
-    #path('')
+    path('', ListarPeliculas.as_view(), name='listar-peliculas'), #mostramos la lista de peliculas
+    path('<int:pk>/', DetallesPelicula.as_view(), name='read-pelicula'), #vemos todos los detalles de la pelicula
+    path('crear/', CrearPelicula.as_view(), name='create-pelicula'), # añadimos una nueva pelicula a a base de datos
+    path('<int:pk>/update/', ActualizarPelicula.as_view(), name='update-pelicula'), # actualizamos la información de una pelicula
+    path('<int:pk>/delete',EliminarPelicula.as_view(), name='delete-pelicula' ), # eliminamos una pelicula de la base de datos
 ]
--- ProyectoCRUD/peliculas/views.py
+++ ProyectoCRUD/peliculas/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Pelicula
+from django.urls import reverse_lazy
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+#vista para mostrar las películas
+class ListarPeliculas(ListView):
+    model = Pelicula
+    template_name = "peliculas/read.html"
+    context_object_name = "peliculas"
+    
+#vista para ver más acerca de la película
+class DetallesPelicula(DetailView):
+    model = Pelicula
+    template_name = 'peliculas/detalles.html'
+    context_object_name = 'pelicula'
+    
+class CrearPelicula(CreateView):
+    model = Pelicula
+    template_name = 'peliculas/create.html'
+    fields = ['titulo', 'director', 'descripcion', 'poster', 'actores_principales', 'fecha_salida', 'genero', 'clasificacion', 'idiomas',
+              'duracion', 'estrellas']
+    success_url = reverse_lazy('listar-peliculas')
+    
+class ActualizarPelicula(UpdateView):
+    model = Pelicula
+    template_name = 'templates/update.html'
+    fields = ['titulo', 'director', 'descripcion', 'poster', 'actores_principales', 'fecha_salida', 'genero', 'clasificacion', 'idiomas',
+              'duracion', 'estrellas']
+    success_url = reverse_lazy('listarPeliculas')
+    
+class EliminarPelicula(DeleteView):
+    model = Pelicula
+    template_name = 'templates/delete.html'
